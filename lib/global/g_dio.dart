1957aec219ced5072d744e605f92049c447ec040
inhohwangg <sk1440sk@naver.com>
2024-12-16 00:24:18 +0900

    dio > 로그인시 변경되는 토큰 최신화할수있게 수정

--- lib/global/g_dio.dart
+++ lib/global/g_dio.dart
@@ -2,10 +2,20 @@ import 'package:apple_highland/main.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-Dio dio = Dio(
+final Dio dio = Dio(
   BaseOptions(
-      baseUrl: dotenv.get('API'),
-      contentType: Headers.formUrlEncodedContentType,
-      validateStatus: (status) => true,
-      headers: {'Authorization': 'Bearer ${getStorage.read('token')}'}),
-);
+    baseUrl: dotenv.get('API'),
+    contentType: Headers.formUrlEncodedContentType,
+    validateStatus: (status) => true,
+  ),
+)..interceptors.add(
+    InterceptorsWrapper(
+      onRequest: (options, handler) {
+        final token = getStorage.read('token');
+        if (token != null) {
+          options.headers['Authorization'] = 'Bearer $token';
+        }
+        handler.next(options);
+      },
+    ),
+  );
